052a43f057d1ae6c4340af91982ed8d8b392ceba
Dillon Mulroy <dillon.mulroy@visionistinc.com>
2017-08-11 10:21:25 -0400

    update cors

--- src/server/index.js
+++ src/server/index.js
@@ -3,14 +3,7 @@ const cors = require('cors');
 const app = express();
 
 // configure app to use CORS.  origin function allows ALL domains so we don't have to whitelist all our servers
-app.use(
-  cors({
-    credentials: true,
-    origin: function(origin, callback) {
-      callback(null, true);
-    }
-  })
-);
+app.use(cors());
 
 app.get('/hello-world', (req, res) => {
   return res.status(200).json({ status: 200, message: 'hello world' });
