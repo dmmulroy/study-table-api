1d859b78e2e739a4d8187c99b31096137438aafd
Dillon Mulroy <dillon.mulroy@visionistinc.com>
2017-08-11 10:14:42 -0400

    add temp cors solution

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+// configure app to use CORS.  origin function allows ALL domains so we don't have to whitelist all our servers
+app.use(
+  cors({
+    credentials: true,
+    origin: function(origin, callback) {
+      callback(null, true);
+    }
+  })
+);
+
 app.get('/hello-world', (req, res) => {
-  return res.json({ status: 200, message: 'hello world' });
+  return res.status(200).json({ status: 200, message: 'hello world' });
 });
 
 module.exports = app;
