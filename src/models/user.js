0c02b625393c115f9c8838dbc4468756c54adca0
Dillon Mulroy <dillon.mulroy@gmail.com>
2017-08-20 18:10:29 -0400

    Update user model to have uuid for the primary key, enforce uniqueness on email + not allow null, not null for password, and create an index on email

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,19 @@
 const User = (db, Sequelize) =>
-  db.define('user', {
-    email: { type: Sequelize.STRING },
-    password: { type: Sequelize.STRING }
-  });
+  db.define(
+    'user',
+    {
+      id: { type: Sequelize.UUID, primaryKey: true },
+      email: { type: Sequelize.STRING, allowNull: false, unique: true },
+      password: { type: Sequelize.STRING, allowNull: false }
+    },
+    {
+      indexes: [
+        {
+          unique: true,
+          fields: ['email']
+        }
+      ]
+    }
+  );
 
 module.exports = User;
