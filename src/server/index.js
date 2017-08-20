36b176549835499d6e4f2f78f193460b5ccefd4d
Dillon Mulroy <dillon.mulroy@gmail.com>
2017-08-20 11:09:03 -0400

    Add support for dynamically adding routes

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,20 @@
+const path = require('path');
+
 const express = require('express');
 const cors = require('cors');
+const dir = require('node-dir');
+
 const app = express();
+const router = express.Router();
 
 app.use(cors());
+
+const apiPath = path.join(__dirname, '..', 'routes', 'api');
+
+dir.files(apiPath, (err, files) => {
+  if (err) throw err;
+  files.forEach(filePath => require(filePath)(router));
+});
 
 app.get('/hello-world', (req, res) => {
   return res.status(200).json({ status: 200, message: 'Hello World' });
