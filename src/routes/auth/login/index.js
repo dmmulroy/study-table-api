c0ee9cd66749fa444136fa4af0e1d0c952aabd45
Dillon Mulroy <dillon.mulroy@gmail.com>
2017-10-08 12:30:16 -0400

    Change post to put

--- src/routes/auth/login/index.js
+++ src/routes/auth/login/index.js
@@ -3,5 +3,5 @@ const UserController = require('../../../controllers/user');
 module.exports = router => {
   router.route('/login').post(UserController.login);
 
-  router.route('/sign-up').post(UserController.create);
+  router.route('/sign-up').put(UserController.create);
 };
